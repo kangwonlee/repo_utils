0b9e21fb9c51f4a759c81ad59ad7f5f77095b1c4
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-06-11 09:34:56 +0900

    added fetch.py

---
+++ fetch.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+
+def main():
+    folders = [item for item in os.listdir(os.curdir) if os.path.isdir(item)]
+
+    for folder in folders:
+        subprocess.check_call(['git', 'fetch', '--all'], cwd=folder)
+
+
+if "__main__" == __name__:
+
+    main()
