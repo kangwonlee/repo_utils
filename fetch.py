77af5c7aa193a0aa08f3a8831abb066fe69aef44
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-06-11 09:48:43 +0900

    refactored folders

--- fetch.py
+++ fetch.py
@@ -4,15 +4,20 @@ import subprocess
 
 
 def main():
-    folders = [item for item in os.listdir(os.curdir) if (
-                os.path.isdir(item) and os.path.exists(os.path.join(item, '.git'))
-            )]
-    random.shuffle(folders)
+    folders = get_folders()
 
     for folder in folders:
 
         print(f"{folder} ".ljust(60, '='))
         subprocess.check_call(['git', 'fetch', '--all'], cwd=folder)
+
+
+def get_folders():
+        folders = [item for item in os.listdir(os.curdir) if (
+                    os.path.isdir(item) and os.path.exists(os.path.join(item, '.git'))
+                )]
+        random.shuffle(folders)
+        return folders
 
 
 if "__main__" == __name__:
