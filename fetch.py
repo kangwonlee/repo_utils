e3f0fefc0bc288edcc046ed5f919d05322514400
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-06-11 09:39:42 +0900

    shuffle folders

--- fetch.py
+++ fetch.py
@@ -1,9 +1,11 @@
 import os
+import random
 import subprocess
 
 
 def main():
     folders = [item for item in os.listdir(os.curdir) if os.path.isdir(item)]
+    random.shuffle(folders)
 
     for folder in folders:
         print(f"{folder} ".ljust(60, '='))
