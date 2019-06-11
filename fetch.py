ae36a08aee3f5bb1183387b64dcbf7faaf497f44
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-06-11 09:37:11 +0900

    indicate folder

--- fetch.py
+++ fetch.py
@@ -6,6 +6,7 @@ def main():
     folders = [item for item in os.listdir(os.curdir) if os.path.isdir(item)]
 
     for folder in folders:
+        print(f"{folder} ".ljust(60, '='))
         subprocess.check_call(['git', 'fetch', '--all'], cwd=folder)
 
 
