f96d4af01014b89814b34b7f2ade262021cecbe7
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-06-11 09:42:19 +0900

    filter git repositories

--- fetch.py
+++ fetch.py
@@ -4,14 +4,16 @@ import subprocess
 
 
 def main():
-    folders = [item for item in os.listdir(os.curdir) if os.path.isdir(item)]
+    folders = [item for item in os.listdir(os.curdir) if (
+                os.path.isdir(item) and os.path.exists(os.path.join(item, '.git'))
+            )]
     random.shuffle(folders)
 
     for folder in folders:
+
         print(f"{folder} ".ljust(60, '='))
         subprocess.check_call(['git', 'fetch', '--all'], cwd=folder)
 
 
 if "__main__" == __name__:
-
     main()
