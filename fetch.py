91b5e699bfcc999521b87582e681f178a402fe98
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2019-06-12 12:53:30 +0900

    implemented subfolder fetcher

--- fetch.py
+++ fetch.py
@@ -4,7 +4,22 @@ import sys
 
 
 def main(argv):
-    pass
+    folder_gen = filter(os.path.isdir, os.listdir(os.getcwd()))
+
+    git_repo_gen = filter(
+        lambda folder: os.path.exists(os.path.join(os.path.join(folder), '.git', 'config')),
+        folder_gen
+    )
+
+    list(
+        map(
+            lambda repo: subprocess.check_call(
+                ['git', 'fetch', '--all']
+                ,cwd=repo
+            ),
+            git_repo_gen
+        )
+    )
 
 
 if "__main__" == __name__:
