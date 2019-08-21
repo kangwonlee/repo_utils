04c15cdb8c901c11cd0135ffb6f08eabf46219c0
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-21 16:45:52 +0900

    added test_get_working_folder_file()

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -19,5 +19,12 @@ def test_get_working_folder_no_arg():
     assert os.getcwd() == result, result
 
 
+def test_get_working_folder_file():
+    result = fetch.get_working_folder([os.path.dirname(__file__)])
+
+    assert isinstance(result, (str, os.PathLike))
+    assert os.path.dirname(__file__) == result, result
+
+
 if "__main__" == __name__:
     pytest.main()
