ed485e06ab14dc1c9413387d16049b90c34de760
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-08-21 18:48:54 +0900

    added folder tree

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,12 +1,12 @@
 import os
+import subprocess
 import sys
 import tempfile
 
 import pytest
 
 sys.path.insert(
-    0,
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 )
 
 import fetch
@@ -30,8 +30,48 @@ def test_get_working_folder_file():
 def umbrella():
     with tempfile.TemporaryDirectory() as folder:
 
+        assert os.path.exists(folder)
+
+        # prepare a test folder
+        sub_folder_00 = tempfile.TemporaryDirectory(dir=folder)
+        sub_folder_01 = tempfile.TemporaryDirectory(dir=folder)
+        sub_folder_git_00 = tempfile.TemporaryDirectory(dir=folder)
+        sub_folder_git_01 = tempfile.TemporaryDirectory(dir=folder)
+
+        temp_file_00 = tempfile.TemporaryFile(dir=folder)
+        temp_file_01 = tempfile.TemporaryFile(dir=folder)
+
+        subprocess.check_call(['git', 'init'], cwd=sub_folder_git_00.name)
+        subprocess.check_call(['git', 'init'], cwd=sub_folder_git_01.name)
+
         # let test use the temp folder
-        yield folder.name
+        yield {
+            "top": folder, 
+            "folder": [
+                sub_folder_00.name,
+                sub_folder_01.name,
+                sub_folder_git_00.name,
+                sub_folder_git_01.name,
+            ],
+            "repos": [
+                sub_folder_git_00.name,
+                sub_folder_git_01.name,
+            ],
+            "files": [
+                temp_file_00,
+                temp_file_01,
+            ]
+        }
+
+        del sub_folder_git_01
+        del sub_folder_git_00
+        del sub_folder_01
+        del sub_folder_00
+
+        # after use, the folder is expected to be removed
+
+    assert not os.path.exists(folder)
+
 
         # after use, remove the folder
         folder.cleanup()
