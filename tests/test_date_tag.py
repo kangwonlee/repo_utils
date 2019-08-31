e5083aee389b4bd419e1543cfc835e3a0d4c5801
KangWon LEE (repo_utils) <kangwon.lee@kpu.ac.kr>
2019-09-01 00:50:41 +0900

    added test_filter_date_tags()

--- tests/test_date_tag.py
+++ tests/test_date_tag.py
@@ -16,5 +16,38 @@ def test_get_tags_list():
     assert isinstance(result, list)
 
 
+@pytest.fixture(scope='module')
+def samples_input():
+    return (
+        "18/02/12/package/00", "18/05/23/package/00", "18/08/12/01", 
+        "18/09/04/01_chapter_order", "18102501", "18103001", "18110501", 
+        "181121_q", "181130/repair_direction_field", "now", "test_matrix", 
+        "181201/posix_nt__each_file", "19040901", "19041001", "19060901", 
+        "19081701", "2nd_int_bug", "2nd_order_int", "60-linalg2/181125/01", 
+        "build/195", "build/249", "cantilever_test", "float_limit", "issue#7", 
+        "issue_#11", "nmisp-20-181201", "nmisp-50-181201", "readme", 
+        "nmisp-50/relative_error", "nmisp/19060901", "nmisp/actions", 
+        "nmisp/badge-workflows", "nmisp/release/test02", "request", 
+        "nmisp/requirements-2019-07", "nmisp/update-workflows-19083101", 
+        "travis", "try_these", "wk00",
+    )
+
+
+@pytest.fixture(scope='module')
+def samples_output():
+    return (
+        "18102501", "18103001", "18110501", "181121_q", 
+        "181130/repair_direction_field",
+        "181201/posix_nt__each_file", "19040901", "19041001", "19060901", 
+        "19081701",
+    )
+
+
+def test_filter_date_tags(samples_input, samples_output):
+    result = dt.filter_date_tags(samples_input)
+
+    assert set(result) == set(samples_output)
+
+
 if "__main__" == __name__:
     pytest.main()
