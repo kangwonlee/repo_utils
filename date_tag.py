2b3f510f1f8b64e77021687b0841caabf262c9bf
KangWon LEE (repo_utils) <kangwon.lee@kpu.ac.kr>
2019-09-01 00:50:31 +0900

    added filter_date_tags()

--- date_tag.py
+++ date_tag.py
@@ -1,9 +1,11 @@
 import os
+import re
 import subprocess
 import sys
 
 
 def main(argv):
+
     assert argv
 
     tags = subprocess.check_output(['git', 'tag'], cwd=argv[0], encoding='utf-8')
@@ -18,6 +20,17 @@ def get_tags_list(folder):
 
     tags = subprocess.check_output(['git', 'tag'], cwd=folder, encoding='utf-8')
     return tags.splitlines()
+
+
+def filter_date_tags(tags_list):
+    result = []
+
+    for tag in tags_list:
+        if tag.startswith("18") or tag.startswith("19"):
+            if '/' != tag[2]:
+                result.append(tag)
+
+    return result
 
 
 if "__main__" == __name__:
