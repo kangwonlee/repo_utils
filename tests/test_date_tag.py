12af961024b703a52bf37be0e3a1b1207735a475
KangWon LEE (repo_utils) <kangwon.lee@kpu.ac.kr>
2019-09-01 00:13:53 +0900

    added test_get_tags_list()

--- tests/test_date_tag.py
+++ tests/test_date_tag.py
@@ -7,4 +7,14 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 )
 
+
 import date_tag as dt
+
+
+def test_get_tags_list():
+    result = dt.get_tags_list(os.path.dirname(__file__))
+    assert isinstance(result, list)
+
+
+if "__main__" == __name__:
+    pytest.main()
