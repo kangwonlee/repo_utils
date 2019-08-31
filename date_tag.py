34bec90b4e3e9cb4922b31fb128dfbffe57197b2
KangWon LEE (repo_utils) <kangwon.lee@kpu.ac.kr>
2019-09-01 01:09:29 +0900

    added convert_date_tag()

--- date_tag.py
+++ date_tag.py
@@ -33,5 +33,19 @@ def filter_date_tags(tags_list):
     return result
 
 
+def convert_date_tag(tag):
+
+    sep = '/'
+
+    assert 6 > len(tag)
+
+    join_this = [tag[:2], tag[2:4], tag[4:6],]
+
+    if 6 < len(tag):
+        join_this.append(tag[6:].strip('_').strip('-').strip(sep))
+
+    return sep.join(join_this)
+
+
 if "__main__" == __name__:
     main(sys.argv[1:])
