1646eb04b45b2d8cb8b1a4b74b87e80f517c357a
KangWon LEE (repo_utils) <kangwon.lee@kpu.ac.kr>
2019-09-14 13:48:33 +0900

    filter duplicated tags

--- list_tags.py
+++ list_tags.py
@@ -4,14 +4,28 @@ import typing
 
 import prefix_tags as pt
 
+Tags = typing.List[str]
+Sha_Tags = typing.Dict[str, Tags]
+
 
 def main(argv: typing.List[str]) -> None:
-
     sha_tags_dict = get_sha_tags_dict()
-    pprint.pprint(sha_tags_dict)
+
+    duplicate_sha_tags = filter_duplicate_tags(sha_tags_dict)
+
+    pprint.pprint(duplicate_sha_tags)
 
 
-def get_sha_tags_dict():
+def filter_duplicate_tags(sha_tags_dict:Sha_Tags) -> Sha_Tags:
+    return {
+        key: value for key, value in filter(
+            lambda sha_tags: 1 < len(sha_tags[-1]),
+            sha_tags_dict.items()
+        )
+    }
+
+
+def get_sha_tags_dict() -> Sha_Tags:
     tag_list = pt.get_tag_list()
 
     tags_dict = {}
