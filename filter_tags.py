61d40c27ef8b61a58f734d76c0b08eb5326e3714
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2019-09-16 13:25:38 +0900

    added process_duplicate_tags()

--- filter_tags.py
+++ filter_tags.py
@@ -4,11 +4,11 @@ import subprocess
 import typing
 import urllib.parse as up
 
+import list_tags as lt
 import prefix_tags as pt
 
 
 def add_remote(url:str, name:str=''):
-
     if not name:
         name = pt.get_repo_name_from_url(url)
 
@@ -27,3 +27,7 @@ def git_fetch_all_tag():
     return pt.run_cmd(
         ['git', 'fetch', '--all', '--tag']
     )
+
+
+def process_duplicate_tags():
+    lt.main([])
