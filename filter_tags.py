e368b64a2c4856ef51b419fafdf85629697e0679
KangWon LEE VISSIM user <kangwon.lee@kpu.ac.kr>
2019-09-16 13:13:45 +0900

    added add_remote()
    type hint for url

---
+++ filter_tags.py
@@ -0,0 +1,18 @@
+import argparse
+import configparser
+import subprocess
+import urllib.parse as up
+
+import prefix_tags as pt
+
+
+def add_remote(url:str, name:str=''):
+
+    if not name:
+        name = pt.get_repo_name_from_url(url)
+
+    r = pt.run_cmd(
+        ['git', 'remote', 'add', name, url]
+    )
+
+    return r
